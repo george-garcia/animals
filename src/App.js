5d4f47eba1428a90ae3cec083d851bde40e0a81a
george-garcia <jorge.a.garcia12@gmail.com>
2022-11-23 09:53:51 -0600

    adding animals
    added animal functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+
+function getRandomAnimal(){
+    const animals = ['bird', 'cat', 'cow', 'dog', 'gator', 'horse'];
+
+    return animals[Math.floor(Math.random() * animals.length)];
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const handleClick = () => {
+
+    };
+
+    return (
+        <div>
+            <button onClick={handleClick}>Add animal</button>
+            
+        </div>
+    );
 }
 
 export default App;
